a50c8c244dbd5d3eaad22a80972b903789df1817
Júlio Mansan <julio.mansan31@gmail.com>
2025-11-12 23:51:59 -0300

    Utilização dos novos assets

--- core/src/main/java/com/seek_knowledge/game/screens/SelectScreen.java
+++ core/src/main/java/com/seek_knowledge/game/screens/SelectScreen.java
@@ -2,43 +2,55 @@ package com.seek_knowledge.game.screens;
 
 import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.seek_knowledge.game.MainGame;
+import com.seek_knowledge.game.sprites.Character;
 import com.seek_knowledge.game.ui.ButtonGame;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class SelectScreen extends GameScreen {
     private ArrayList<ButtonGame> characterButtons;
-
-    String[] characterNames = { "Dio_Brando", "Jotaro_Kujo", "Joseph_Joestar", "Giorno_Giovanna" };
+    private World world;
+    String[] characterNames = { "Samurai", "Satiro", "Astronomo", "Paladino" };
+    private ArrayList<Character> characters;
 
     public SelectScreen(MainGame game) {
-        super(game, "images.atlas", "Dio_Brando");
+        super(game, "assets/assets.atlas", "Background_Select");
         Gdx.input.setInputProcessor(stage);
-
-        characterButtons = new ArrayList<ButtonGame>();
-
+        this.world = new World(new Vector2(0, -10), true);
+        this.characterButtons = new ArrayList<ButtonGame>();
+        this.characters = new ArrayList<Character>();
+        
         for (String name : characterNames) {
-            ButtonGame button = new ButtonGame(name, "assets.atlas", "Button_Unpressed", "Button_Pressed");
+            ButtonGame button = new ButtonGame(name, "assets/assets.atlas", "Button_Unpressed", "Button_Pressed");
             characterButtons.add(button);
         }
 
         Table table = new Table();
         table.setFillParent(true);
-        table.bottom();
+        table.center().bottom();
         table.padBottom(100);
 
+        float spacing = WORLD_WIDTH/(characterNames.length);
+
         for (int i = 0; i < characterButtons.size(); i++) {
-            table.add(characterButtons.get(i).getButton()).padRight(50).size(360, 120);
+            table.add(characterButtons.get(i).getButton()).expandX().size(360, 120);
+            float x = spacing * (i + 0.5f);
+            characters.add(new Character(world, "characters/" + characterNames[i] + ".atlas", x, 46f, 0.1f));
         }
         stage.addActor(table);
 
         for (int i = 0; i < characterButtons.size(); i++) {
+            final int index = i;
             characterButtons.get(i).getButton().addListener(new ClickListener() {
+                String name = characterNames[index];
+
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
-                    game.setScreen(new MainScreen(game));
+                    game.setScreen(new MainScreen(game, name));
                     dispose();
                 }
             });
@@ -48,6 +60,13 @@ public class SelectScreen extends GameScreen {
     @Override
     public void render(float delta) {
         super.render(delta);
+        super.batch.begin();
+        for (Character character : characters) {
+            character.update(delta, 1f, 0.2f);
+            character.draw(batch);
+        }
+        super.batch.end();
+
         stage.act(delta);
         stage.draw();
     }
